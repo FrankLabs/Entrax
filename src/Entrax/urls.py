21a442ca555798505899bda1a12a89277fda0ba2
Pablo N Dalmasso <pdalmasso@MacBook-Pro-de-Pablo.local>
2014-11-29 12:31:06 -0300

    "Se agregan primeras vistas"

--- src/Entrax/urls.py
+++ src/Entrax/urls.py
@@ -7,5 +7,21 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^prueba/', 'apps.rider.views.prueba')
+    url(r'^prueba/', 'apps.rider.views.prueba'),
+    url(
+        r'^hola_mundo/$',
+        'apps.championship.views.hola_mundo'
+    ),
+    url(
+        r'^saludo/(?P<nombre>[a-zA-Z@.-_]+)/',
+        'apps.championship.views.hola_mundo'
+    ),
+    url(
+        r'^championship_list/$',
+        'apps.championship.views.championship_list'
+    ),
+    url(
+        r'^championship_list/(?P<discipline_parametro>\d+)/',
+        'apps.championship.views.championship_list'
+    )
 )
