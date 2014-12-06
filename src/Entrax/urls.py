e837b376165669809f3bcf0c3d2e5ef5b5b8469b
Alumno <alumno@debian-curso>
2014-12-06 12:55:30 -0300

    urls de LOGIN

--- src/Entrax/urls.py
+++ src/Entrax/urls.py
@@ -7,6 +7,10 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout',
+        {'next_page': '/'}
+    ),
     url(r'^prueba/', 'apps.rider.views.prueba'),
     url(
         r'^hola_mundo/$',
